b350a7fd63237df74c35a8a6d406a6e53b84fc21
Guillaumefe <guillaume.ferron@gmail.com>
2021-05-09 22:51:25 +0200

    right to left binding (not working)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import yaml from "js-yaml";
 
 import AceEditor from "react-ace";
+import { Range } from "ace-builds"
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-github";
 
@@ -75,9 +76,10 @@ function App() {
         target.await = false
         target.cancel = false
         target.later = false
-        target.value = target.value + ' --'
+        //target.value = target.value + ' --'
         if ( ! isDone ) {
             target.done = true;
+            writeLine(target.key, target.value, '--')
         //} else {
         //    target.done = false;
         }
@@ -126,43 +128,48 @@ function App() {
         setValue([...value])
     }
 
-    const loadYaml = () => new Promise((onResolve, onReject)=>{
+    const loadPipeline = (doc) => {
         let cout = 0
+        const output = deepReduce(doc, (memo, value, path) => {
+            if (value && typeof value === "string") {
+                const task = {
+                    key: cout++,
+                    done: false,
+                    later: false,
+                    cancel: false,
+                    await: false,
+                    path: path.map( x => (isNaN(x)) ? x : ""),
+                    value
+                }
+                memo.push(task)
+            }
+            return memo
+        }, []);
+
+        return output
+    }
+
+    const loadYaml = (newValue) => new Promise((success, failure) => {
         try {
-            yaml.loadAll(load(), function (doc) {
-                const output = deepReduce(doc, (memo, value, path) => {
-                    if (value && typeof value === "string") {
-                        const task = {
-                            key: cout++,
-                            done: false,
-                            later: false,
-                            cancel: false,
-                            await: false,
-                            path: path.map( x => (isNaN(x)) ? x : ""),
-                            value
-                        }
-                        memo.push(task)
-                    }
-                    return memo
-                }, []);
-                onResolve(output)
-            });
+            yaml.loadAll(newValue, function (doc) {
+                const pl = loadPipeline(doc)
+                success(pl)
+                //save(doc)
+            })
         } catch(e) {
-            onReject(e)
+            failure(e)
         }
     })
 
     const onChange = (newValue) => {
-
         save(newValue)
-        loadYaml()
-            .then( v => {
-                setValue(v)
+        loadYaml(newValue)
+            .then( pl => {
+                setValue(pl)
             })
             .catch( e => {
-                setValue([])
+                setValue(e.message)
             })
-
     }
 
     const save = (pl) => {
@@ -173,14 +180,53 @@ function App() {
         return JSON.parse(localStorage.getItem('pl'))
     }
 
+    const Ed = React.createRef();
+
+    const writeLine = (line, pattern, sign) => {
+        const session = Ed.current.editor.session
+        const content = session.getValue()
+        let current_line = 0
+        let accurate_line = 0
+        const pl = content.split('\n').map( (x,i) => {
+            if (x && x[x.length-1] !== ":" && x[x.length-1] !== ">" && x[x.length-1] !== "|") {
+                if (current_line === line && x.trim()[0] === "-") {
+                    session.replace(new Range(accurate_line, 0, accurate_line, Number.MAX_VALUE), x + " --")
+                    current_line++
+                } else if (current_line === line && x.trim()[0] === "-") {
+                    accurate_line++
+                }
+            }
+            accurate_line++
+        //    if (i === line)
+        //        return x + " TOTOTOOKOKOKOK"
+        //    else
+        //        return x
+        //    if (x) {
+        //        if (current_line  === line)
+        //            session.replace(new Range(accurate_line, Number.MAX_VALUE, accurate_line, Number.MAX_VALUE), x + " --")
+        //        current_line++
+        //    }
+        //    accurate_line++
+        })
+        //save(pl.join('\n'))
+        //alert(line)
+}
+
     const onLoad = (editor) => {
         const session = editor.session
+        //let textContent = yaml.dump(load(), {
+        //    'styles': {
+        //        '!!null': 'canonical' // dump null as ~
+        //    },
+        //    'sortKeys': true        // sort object keys
+        //});
         let textContent = load() || ""
         editor.focus();
         session.insert({
             row: session.getLength(),
             column: 0
-        }, load())
+        }, textContent)
+
     }
 
   //const html = '<textarea id="result" style={{flex: 1, border: "none", overflow: "auto", outline: "none"}} disabled={true}></textarea>' 
@@ -191,6 +237,7 @@ function App() {
       <header className="App-header">
       </header>
       <AceEditor style={{flex: 1}}
+      ref={Ed}
       placeholder="Read the doc : https://guillaumefe.github.io/linrd/"
       maxLines= 'Infinity'
       mode="java"
@@ -201,17 +248,19 @@ function App() {
       onLoad = {onLoad}
       />
       <ListGroup id="result" style={{flex: 1, padding: "10px"}}>
-        {value.map( x => {
-          return <ListGroup.Item style={{display: "flex"}}>
-                <div style={{flex: 1}}>{"[" + x.key +  "]"} {x.path.join(" ") + x.value}</div>
-                <div style={{flex:0.1}}>
-                    <Button data-key={x.key} variant={(x.later) ? "warning" : "outline-warning"} size="sm" style={{width:"60px", marginBottom: "1px"}} onClick={onLater}>Later</Button>
-                    <Button data-key={x.key} variant={(x.done) ? "success" : "outline-success"} size="sm" style={{width:"60px", marginBottom: "1px"}} onClick={onDone}>Done</Button>
-                    <Button data-key={x.key} variant={(x.await) ? "primary" : "outline-primary"} size="sm" style={{width:"60px", marginBottom: "1px"}} onClick={onAwait}>Await</Button>
-                    <Button data-key={x.key} variant={(x.cancel) ? "info" : "outline-info"} size="sm" style={{width:"60px", marginBottom: "1px"}} onClick={onCancel}>Cancel</Button>
-                </div>
-              </ListGroup.Item>
-        })}
+        {
+            ( ! value.map) ? "ERROR : " + value.toString() : value.map( x => {
+                    return <ListGroup.Item style={{display: "flex"}}>
+                        <div style={{flex: 1}}>{"[" + x.key +  "]"} {x.path.join(" ") + x.value}</div>
+                        <div style={{flex:0.1}}>
+                        <Button data-key={x.key} variant={(x.later) ? "warning" : "outline-warning"} size="sm" style={{width:"60px", marginBottom: "1px"}} onClick={onLater}>Later</Button>
+                        <Button data-key={x.key} variant={(x.done) ? "success" : "outline-success"} size="sm" style={{width:"60px", marginBottom: "1px"}} onClick={onDone}>Done</Button>
+                        <Button data-key={x.key} variant={(x.await) ? "primary" : "outline-primary"} size="sm" style={{width:"60px", marginBottom: "1px"}} onClick={onAwait}>Await</Button>
+                        <Button data-key={x.key} variant={(x.cancel) ? "info" : "outline-info"} size="sm" style={{width:"60px", marginBottom: "1px"}} onClick={onCancel}>Cancel</Button>
+                        </div>
+                        </ListGroup.Item>
+                })
+        }
       </ListGroup>
     </div>
   );
