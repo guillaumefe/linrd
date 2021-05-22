2a7c8a990e652aa2fbd54d8294a47f6ed1167e3f
Guillaumefe <guillaume.ferron@gmail.com>
2021-05-23 01:36:39 +0200

    ignore persist serialization

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 import pipelinrReducer from '../features/pipelinr/pipelinrSlice';
+
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
 
 const persistConfig = {
   key: 'root',
@@ -16,6 +25,11 @@ export const store = configureStore({
   reducer: {
     pipelinr: persistedReducer,
   },
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
 });
 
 export const persistor = persistStore(store)
