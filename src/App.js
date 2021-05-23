38598113a955491729663e8ac1aa5125f26264f7
Guillaumefe <guillaume.ferron@gmail.com>
2021-05-23 15:07:05 +0200

    add quick start

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
 import React from 'react';
 import { Editor } from './features/pipelinr/Pipelinr';
 import { Viewer } from './features/pipelinr/Pipelinr';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App" style={{ display:"flex", minHeight:"40em" }}>
+
         <Editor />
         <Viewer style={{flex: 1, padding: "10px"}} />
+
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
