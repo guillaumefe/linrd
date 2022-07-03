2ab4e47769908e73858f363fd2c45b38722b6a00
manager <manager@attackr.lan>
2022-07-03 09:48:18 +0200

    ameliorates search, remove shadow bug where searchterm was not displayed in the searchbar after reload

--- src/components/navigation/Menu.js
+++ src/components/navigation/Menu.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useSelector, useDispatch } from 'react-redux';
 import {
+  selectSearchTerm,
   updateSearchTerm
 } from '../helpers/Reducer';
 
@@ -12,13 +13,15 @@ import {
     FormControl,
 } from "react-bootstrap"
 
-
 export const Menu = () => {
+
 
     const dispatch = useDispatch()
     const onChange = (event) => {
         dispatch(updateSearchTerm(" " + event.target.value))
     }
+
+    const searchterm = useSelector(selectSearchTerm)
 
     return (
         <Navbar bg="light" expand="lg" style={{padding:"10px", display: "flex", justifyContent: "space-between"}}>
@@ -26,7 +29,7 @@ export const Menu = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Form inline style={{flex:1}}>
-        <FormControl onChange={onChange} type="text" placeholder="Search" className="mr-sm-2" />
+        <FormControl onChange={onChange} value={searchterm} type="text" placeholder="Search" className="mr-sm-2" />
         </Form>
         </Navbar.Collapse>
         </Navbar>
